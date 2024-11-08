37eeb4a7b197d9fd20c1993deb638335e86323b0
Kolozsi Márton <kolozsi.ocsi@gmail.com>
2024-11-08 17:14:29 +0100

    A settings.js closeSettings metódus hozzáadása

--- docs/scripts/settings.js
+++ docs/scripts/settings.js
@@ -2,3 +2,8 @@ function openSettings() {
     document.getElementById("settingsModal").style.display = "flex";
     document.getElementById("settingsModal").scrollTop = 0;
 }
+
+function closeSettings() {
+    applySettings();
+    document.getElementById("settingsModal").style.display = "none";
+}
