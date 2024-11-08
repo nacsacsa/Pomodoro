288aa824822f638a71cd187c89cf253fcf2b1c9f
HorogGabriella <gabriellahorog6@gmail.com>
2024-11-08 20:15:24 +0100

    index.php head

--- docs/backend/index.php
+++ docs/backend/index.php
@@ -1,1 +1,16 @@
+
 <?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pomodoro Timer</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+</html>
