04a7d708dea986ac2a567e799b7ed522ff36f816
HorogGabriella <gabriellahorog6@gmail.com>
2024-11-08 20:20:11 +0100

    index.php body rész navbar

--- docs/backend/index.php
+++ docs/backend/index.php
@@ -13,4 +13,21 @@ session_start();
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/style.css">
 </head>
+
+<body>
+<header class="navbar">
+    <div class="nav-buttons">
+        <button onclick="openSettings()"><i class="bi bi-gear-fill"></i>&nbspBeállítások</button>
+        <?php if (isset($_SESSION['user_id'])): ?>
+            <a href="profile.php" id="profileButton"><i class="bi bi-person-fill"></i>&nbspFiókom</a>
+            <a href="logout.php" id="logoutButton"><i class="bi bi-box-arrow-left"></i>&nbspKijelentkezés</a>
+        <?php else: ?>
+            <a href="../signUp.html" id="signUpButton"><i class="bi bi-person-fill"></i>&nbspRegisztrálás</a>
+            <a href="../login.html" id="loginButton"><i class="bi bi-box-arrow-in-right"></i>&nbspBejelentkezés</a>
+        <?php endif; ?>
+
+        <button id="menuButton" onclick="toggleDropdown()"><i class="bi bi-three-dots-vertical"></i></button>
+    </div>
+</header>
+</body>
 </html>
