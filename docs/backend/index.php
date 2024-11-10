e971bf9070fd47d9615563c2159fce29b2bfd704
Délczeg Sándor Balázs <balazsdelczeg@gmail.com>
2024-11-10 17:22:45 +0100

    index.php css referencia kijavítása

--- docs/backend/index.php
+++ docs/backend/index.php
@@ -10,7 +10,7 @@ session_start();
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../styles/style.css">
 </head>
 <body>
 <header class="navbar">
