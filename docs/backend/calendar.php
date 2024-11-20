98da88f4b3acaae6e917b413d8d6da81e9c1e7b9
regi-hrg <reginahorog4@gmail.com>
2024-11-20 12:08:38 +0100

    calendar.php html rész

--- docs/backend/calendar.php
+++ docs/backend/calendar.php
@@ -20,3 +20,36 @@ while ($row = $result->fetch_assoc()) {
 
 $pomodoroDataJson = json_encode($pomodoroData);
 ?>
+
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pomodoro - Naptár</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../styles/calendar.css">
+</head>
+<body>
+
+<div class="navbar">
+    <a href="index.php">Főoldal</a>
+    <a href="logout.php">Kijelentkezés</a>
+</div>
+
+<div class="calendar-container">
+    <div class="navigation">
+        <button id="prev-month"><i class="bi bi-arrow-left-square-fill"></i></button>
+        <h2 id="current-month"></h2>
+        <button id="next-month"><i class="bi bi-arrow-right-square-fill"></i></button>
+    </div>
+
+    <div class="calendar" id="calendar"></div>
+</div>
+<script id="pomodoro-data" type="application/json">
+    <?php echo $pomodoroDataJson; ?>
+</script>
+
+</body>
+</html>
