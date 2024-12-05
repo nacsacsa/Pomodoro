9219b3bfb9f5d6638c687a29c0cc59d8663f78b2
regi-hrg <reginahorog4@gmail.com>
2024-12-05 20:36:28 +0100

    admin.php php rész megírása

--- docs/backend/admin.php
+++ docs/backend/admin.php
@@ -1,1 +1,12 @@
 <?php
+session_start();
+include 'database.php';
+
+if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
+    header("Location: index.php");
+    exit;
+}
+
+$sql = "SELECT id, name, email, role FROM Users";
+$result = $conn->query($sql);
+?>
